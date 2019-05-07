3d808fc72931b4b41bcf97685eca7a531e86515c
Naomi Katz <naykatz@gmail.com>
2019-05-07 11:42:53 -0400

    backend

--- app/controllers/api/bumblefooks_controller.rb
+++ app/controllers/api/bumblefooks_controller.rb
@@ -5,4 +5,20 @@ class Api::BumblefooksController < ApplicationController
     render json: @bumblefooks
   end
 
+  def create
+      @bumblefook = Bumblefook.create(bumblefook_params)
+      render json: @bumblefook
+    end
+
+
+
+
+
+
+  private
+
+  def bumblefook_params
+  params.permit(:name, :description, :location, :image_url)
+  end
+
 end
